1412081b544779cfde413d71361e376c4ba56644
Jwhyee <82663161+Jwhyee@users.noreply.github.com>
2021-12-06 13:08:25 +0900

    메인 화면에 저장된 DB 뿌리기 가능

---
+++ app/src/main/java/com/example/mobile_project_tripper/ListViewAdapter_main.java
@@ -0,0 +1,76 @@
+package com.example.mobile_project_tripper;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ListViewAdapter_main extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    Context mContext1;
+    private ArrayList<DiaryItem_main> IvList1;
+    int Position1;
+
+    public ListViewAdapter_main(ArrayList<DiaryItem_main> items1, Context context1) {
+        IvList1 = items1;
+        mContext1 = context1;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_item_list, parent, false);
+        return new ViewHolder1(view);
+    }
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder1, int position1) {
+        // ListView의 getView 부분을 담당하는 메소드
+        ((ViewHolder1) holder1).onBind1(IvList1.get(position1));
+    }
+
+    @Override
+    public int getItemCount() {
+        return IvList1.size(); // 데이터 개수 리턴
+    }
+
+    public class ViewHolder1 extends RecyclerView.ViewHolder {
+        public TextView d_no_contain;
+        public TextView d_title_contain;
+        public TextView d_location_contain;
+        public TextView d_start_date_contain;
+        public TextView d_end_date_contain;
+
+        public ViewHolder1(View itemView) {
+            super(itemView);
+            // 화면에 표시될 View 로부터 위젯에 대한 참조 획득
+            d_title_contain = itemView.findViewById(R.id.d_title_contain);
+            d_location_contain = itemView.findViewById(R.id.d_location_contain);
+            d_start_date_contain = itemView.findViewById(R.id.d_start_date_contain);
+            d_end_date_contain = itemView.findViewById(R.id.d_end_date_contain);
+            d_no_contain = itemView.findViewById(R.id.d_no_contain);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Position1 = getAdapterPosition();
+                    Intent intent = new Intent(mContext1, ViewNote.class);
+                    intent.putExtra(mContext1.getString(R.string.row_id),IvList1.get(Position1).getUid());
+                    Log.e("rowID", String.valueOf(IvList1.get(Position1).getUid()));
+                    mContext1.startActivity(intent);
+                }
+            });
+        }
+
+        public void onBind1(DiaryItem_main item) {
+            d_title_contain.setText(item.getTitle());
+            d_location_contain.setText(item.getLocate());
+            d_start_date_contain.setText(item.getStartDate());
+            d_end_date_contain.setText(item.getEndDate());
+        }
+    }
+}
--- app/src/main/java/com/example/mobile_project_tripper/MainActivity.java
+++ app/src/main/java/com/example/mobile_project_tripper/MainActivity.java
@@ -5,19 +5,24 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<DiaryList> profileDataArray = new ArrayList<>();
     DBHelper helper;
     SQLiteDatabase db;
+    private RecyclerView listView_main;
+    private ArrayList<DiaryItem_main> diaryItemList_main = new ArrayList<>(); // SQLite에서 가져온 원본 데이터 리스트
+    RecyclerView.Adapter listViewAdapter_main; // ListViewAdapter 대신 RecyclerView.Adapter
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,42 +30,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //DB 추가
 
+        listView_main = (RecyclerView)findViewById(R.id.diary_list);
+        listView_main.setHasFixedSize(true);
+        diaryItemList_main.clear(); // 가져온 데이터 초기화
         helper = new DBHelper(this);
-        db = helper.getWritableDatabase();
-        helper.onCreate(db);
-        RecyclerView rv_profile = findViewById(R.id.diary_list);
-        RecyclerView.LayoutManager myLayoutManager = new LinearLayoutManager(this);
-        // 레이아웃 매니져를 LinearLayoutManager로 생성
+        db= helper.getReadableDatabase();
+        db.beginTransaction();
 
-        rv_adapter myAdapter = new rv_adapter(profileDataArray);
-        rv_profile.setLayoutManager(myLayoutManager);
-        // 생성한 myLayoutManager 객체를 rv_profile 리사이클러뷰의 레이아웃매니져로 설정
-        rv_profile.setAdapter(myAdapter);
-        // 생성한 myAdapter 객체를 rv_profile의 리사이클러뷰의 어댑터로 설정
+        Cursor cursor1 = helper.LoadSQLiteDBCursor2();
 
-        // item(row)추가방법2. 메소드를 통해 객체를 생성하고 profileDataArray에 추가하는 방법 => 해당 객체가 row(item)으로 출력됨
-        makeNewDataObject("제목","지역","2021-12-03");
+        try {
+            cursor1.moveToFirst();
+            System.out.println("SQLiteDB 개수 = " + cursor1.getCount());
+            while (!cursor1.isAfterLast()) {
+                addGroupItem(cursor1.getString(0),cursor1.getString(1),
+                        cursor1.getString(2),cursor1.getString(3));
+                cursor1.moveToNext();
+            }
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor1 != null) {
+                cursor1.close();
+                db.endTransaction();
+            }
+        }
 
-        // 객체가 추가된 ArrayList를 출력하기위해, 리사이클러뷰 어댑터 최신화
-        myAdapter.notifyDataSetChanged();
+        // 레이아웃 매니저 세팅
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        listView_main.setLayoutManager(layoutManager);
 
-        /*
-        //db insert
-        String sql = "INSERT INTO mytable('txt') values('Bulgogi');";
-        db.execSQL(sql);
-
-        // 리사이클러뷰 db select
-        RecyclerView diary_list = (RecyclerView)findViewById(R.id.diary_list);
-
-        String sql2 = "SELECT * FROM mytable;";
-        Cursor c = db.rawQuery(sql2,null);
-        String[] strs = new String[]{"txt"};
-        int[] ints = new int[]{R.id.listview_txt};
-
-        SimpleCursorAdapter adapter = null;
-        adapter = new SimpleCursorAdapter(diary_list.getContext(), R.layout.listview, c, strs, ints, 0);
-        diary_list.setAdapter(adapter);
-         */
+        listViewAdapter_main = new ListViewAdapter_main(diaryItemList_main, this); // Adapter 생성
+        listView_main.setAdapter(listViewAdapter_main); // 어댑터를 리스트뷰에 세팅
 
         // 캘린더 이미지 클릭 시 화면 전환
         ImageView calendarImg = (ImageView)findViewById(R.id.calendarImg);
@@ -84,16 +87,13 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void makeNewDataObject(String title, String location, String date) {
-        // ArrayList에 추가할 rv_profile_data 객체를 생성한 뒤,
-        DiaryList newDataObject = new DiaryList();
 
-        // 생성자로 받은 parameter들을 rv_profile_data 클래스 set 메소드를 이용하여 이름, 나이, 연락처, 좋아하는 것을 설정해줍니다.
-        newDataObject.setTitle1(title);
-        newDataObject.setLocation(location);
-        newDataObject.setDate(date);
-
-        // 만든 데이터 객체를 ArrayList에 추가합니다. 추가된 데이터는 하나의 item(row)로 출력됩니다.
-        profileDataArray.add(newDataObject);
+    public void addGroupItem(String title, String Locate, String sDate, String eDate){
+        DiaryItem_main item = new DiaryItem_main();
+        item.setTitle(title);
+        item.setLocate(Locate);
+        item.setStartDate(sDate);
+        item.setEndDate(eDate);
+        diaryItemList_main.add(item);
     }
 }
